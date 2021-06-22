803dc757a61b875d8f3071ebd5069a478c85314e
Syed Huzaifa Ali <80565640+syedhuzaifaali660@users.noreply.github.com>
2021-06-22 22:48:57 +0500

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<SingletonClass> : MonoBehaviour where SingletonClass: Component
+{
+    private static SingletonClass _instance;
+    public static SingletonClass Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<SingletonClass>();
+                if(_instance == null)
+                {
+                    GameObject GO = new GameObject();
+                    _instance = GO.AddComponent<SingletonClass>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+ protected virtual void Awake()
+    {
+        _instance = this as SingletonClass;
+    }
+}
